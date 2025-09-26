068162617331de3adf8af48789e5f16a49ac5361
Sahul Hameed <sahulhameed9094@gmail.com>
2025-09-26 15:21:55 +0530

    Add project files.

---
+++ MVCWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            return View(products);
+        }
+    }
+}
